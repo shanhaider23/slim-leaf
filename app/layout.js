3075ecf7596b12352abc66431142d978bfa04907
shan-e-haider bukhari <shanhaider23@lenovo-home.localdomain>
2025-09-25 13:53:09 +0200

    added product pics

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -58,6 +59,7 @@ export default function RootLayout({ children }) {
       >
         {children}
       </body>
+      <Analytics />
     </html>
   );
 }
